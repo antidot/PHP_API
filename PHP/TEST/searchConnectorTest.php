8b8d80684aafa7dfb2ae77f98435d0ea263a654c
ml <mlorente@antidot.net>
2013-10-23 16:17:12 +0200

    Fix unittest

--- PHP/TEST/searchConnectorTest.php
+++ PHP/TEST/searchConnectorTest.php
@@ -3,14 +3,14 @@ require_once "afs_search_connector.php";
 
 class Connector extends AfsSearchConnector
 {
-    public function __construct($search_url, $service)
+    public function __construct($host, $service)
     {
-        parent::__construct($search_url, $service);
+        parent::__construct($host, $service);
     }
 
     public function get_url()
     {
-        return $this->search_url;
+        return $this->scheme . '://' . $this->host . '/search';
     }
     public function get_id()
     {
@@ -31,14 +31,14 @@ class SearchConnectorTest extends PHPUnit_Framework_TestCase
     public function testConstructDefaultParameters()
     {
         $connector = new Connector('url', new AfsService(42));
-        $this->assertTrue($connector->get_url() == 'url');
+        $this->assertEquals($connector->get_url(), 'http://url/search');
         $this->assertTrue($connector->get_id() == 42);
         $this->assertTrue($connector->get_status() == 'stable');
     }
     public function testConstructParameters()
     {
         $connector = new Connector('url', new AfsService(42, 'rc'));
-        $this->assertTrue($connector->get_url() == 'url');
+        $this->assertEquals($connector->get_url(), 'http://url/search');
         $this->assertTrue($connector->get_id() == 42);
         $this->assertTrue($connector->get_status() == 'rc');
     }
