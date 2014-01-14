ce50c3aa651268ac9352b0122849e2da27632121
ml <mlorente@antidot.net>
2014-01-14 10:30:54 +0000

    Add support for agent CONCEPT

--- TEST/responseHelperTest.php
+++ TEST/responseHelperTest.php
@@ -229,6 +229,81 @@ class ResponseHelperTest extends PHPUnit_Framework_TestCase
             $this->fail('Should not have found any appropriate reply set');
         } catch (OutOfBoundsException $e) { }
     }
+
+    public function testRetrieveConcept()
+    {
+        $input = json_decode('{
+                "header": {
+                    "query": {
+                        "userId": "foo",
+                        "sessionId": "bar"
+                    },
+                    "user": { },
+                    "performance": {
+                        "durationMs": 211
+                    },
+                    "info": { }
+                },
+                "replySet": [
+                    {
+                      "meta": {
+                          "uri": "Default",
+                          "totalItems": 1,
+                          "totalItemsIsExact": true,
+                          "pageItems": 1,
+                          "firstPageItem": 1,
+                          "lastPageItem": 1,
+                          "durationMs": 0,
+                          "firstPaFId": 1,
+                          "lastPaFId": 1,
+                          "producer": "CONCEPT"
+                      },
+                      "content": {
+                          "reply": [
+                              {
+                                  "docId": 1,
+                                  "uri": "concept",
+                                  "concept": {
+                                      "query": {
+                                          "items": [
+                                              {
+                                                  "afs:t": "QueryMatch",
+                                                  "text": "mariage",
+                                                  "uri": [ "lnf:taxo#QI-thm2009862" ]
+                                              }
+                                          ]
+                                      },
+                                      "concepts": {
+                                          "concept": [
+                                              {
+                                                  "uri": "lnf:taxo#QI-thm2009862",
+                                                  "contents": "foo"
+                                              }
+                                          ]
+                                      }
+                                  }
+                              }
+                          ]
+                      }
+                  }
+                ]
+            }');
+
+        $facet_mgr = new AfsFacetManager();
+        $query = new AfsQuery();
+        $response = new AfsResponseHelper($input, $facet_mgr, $query, null, AFS_HELPER_FORMAT);
+        
+        $this->assertTrue($response->has_concept());
+        $concept_helper = $response->get_concept('Default');
+        $concept_items = $concept_helper->get_items();
+        $this->assertEquals(1, count($concept_items));
+        $concept_item = each($concept_items);
+        $concept_item = $concept_item['value'];
+        $this->assertEquals('mariage', $concept_item->get_text());
+        $item = each($concept_item->get_data());
+        $this->assertEquals('foo', $item['value']);
+        $this->assertEquals('lnf:taxo#QI-thm2009862', $item['key']);
+    }
 }
 
 ?>
