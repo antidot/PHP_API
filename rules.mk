8e14dd92be61ca5b766114d3a8a91cc777c408a5
ml <mlorente@antidot.net>
2014-01-28 14:25:30 +0000

    Add Makefiles so that all tests can be launched from the root folder

---
+++ rules.mk
@@ -0,0 +1,54 @@
+#*******************************************************************************
+#
+# AFS © Antidot 2014
+#
+#*******************************************************************************
+.PHONY: all_tests test individual_tests
+.PHONY: local_test local_individual_tests recurse
+
+
+TEST_DIR=$(filter %/TEST, $(CURDIR))
+
+
+all_tests: test individual_tests
+
+test: local_test recurse
+
+individual_tests: local_individual_tests recurse
+
+local_test:
+	@if [ -f $(which phpunit) ]; \
+	then \
+		if [ "${TEST_DIR}" != "" ]; \
+		then \
+			phpunit --stop-on-failure --include-path $(ROOT_PATH) $(CURDIR) || exit 1; \
+		fi; \
+	else \
+		echo 'You need phpunit installed on your computer to run unit tests'; \
+	fi
+
+local_individual_tests:
+	@if [ -f $(which phpunit) ]; \
+	then \
+		if [ "${TEST_DIR}" != "" ]; \
+		then \
+			for file in $$(ls *.php); \
+			do \
+				echo "\n=============================================="; \
+				echo ">>>> Running tests for $${file} <<<<"; \
+				echo "=============================================="; \
+				phpunit --stop-on-failure --include-path $(ROOT_PATH) $${file} || exit 1; \
+			done; \
+		fi; \
+	else \
+		echo 'You need phpunit installed on your computer to run unit tests'; \
+	fi
+
+recurse:
+	@for DIR in $$(ls -d */ 2> /dev/null); \
+	do \
+		if [ -f "$${DIR}/Makefile" ]; \
+		then \
+			$(MAKE) -C $${DIR} $(MAKECMDGOALS) || exit 1; \
+		fi; \
+	done
