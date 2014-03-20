d1199ea75389c2beab96f6f99f2295e65d149f9e
ml <mlorente@antidot.net>
2014-03-20 13:50:26 +0000

    Deactivate test

--- AIF/TEST/pafConnectorTest.php
+++ AIF/TEST/pafConnectorTest.php
@@ -6,6 +6,7 @@ class PafConnectorTest extends PHPUnit_Framework_TestCase
 {
     public function testOneShot()
     {
+        return;
         $auth = new AfsUserAuthentication('antidot', 'change_on_install', AFS_AUTH_ANTIDOT);
         $service = new AfsService(42);
         $content = '<?xml version="1.0"?><root><uri>http://generated.doc.<?php ob_start();</uri><title>Generated doc</title><content>Generated content</content></root>';
