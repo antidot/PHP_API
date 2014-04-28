e5d40ebf348f294d4516b326ce2b7cd5c65194b8
Antoine Gaillard <agaillard@antidot.net>
2014-04-28 18:33:38 +0200

    Fix

--- AIF/TEST/documentManagerTest.php
+++ AIF/TEST/documentManagerTest.php
@@ -30,7 +30,8 @@ class DocumentManagerTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($mgr->has_document());
         $this->assertEquals(count($mgr->get_documents()), 1);
         $docs = $mgr->get_documents();
-        $filename = reset($docs)->get_filename();
+				$doc = reset($docs);
+        $filename = $doc->get_filename();
         $this->assertEquals(file_get_contents($filename), 'baz');
     }
 
